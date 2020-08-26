ac327bcee1b23ce565b111d5dbc9211593dc7033
chaeyoon.kim <yoonchae125@naver.com>
2020-08-26 22:07:42 +0900

    kakao 2020 blind: 괄호변환

---
+++ Practice/src/kakao_2020_blind/Main.kt
@@ -0,0 +1,9 @@
+package kakao_2020_blind
+
+fun main(args: Array<String>) {
+//    val num1 = StringShorttening().solution("a")
+//    println(num1)
+
+    val num2 = Gualho().solution("()))((()")
+    println("ans : $num2")
+}
