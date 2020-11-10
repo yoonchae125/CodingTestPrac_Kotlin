3531aa9105fe1132e885527534155ad5874a3fac
chaeyoon <yoonchae125@naver.com>
2020-11-10 14:59:07 +0900

    Flyod Warshall Algorithm

--- Practice/src/Zip.java
+++ Practice/src/Zip.java
@@ -80,7 +80,7 @@ public class Zip {
         } else if (x == 2) {
             return true;
         }
-        for (int i=2; i*i <= x; i++) {
+        for (int i = 2; i * i <= x; i++) {
             if (x % i == 0) {
                 return false;
             }
@@ -88,13 +88,55 @@ public class Zip {
         return true;
     }
 
-    static class KruskalMST{
-        public static int solution(int n, int[][] costs){
+    public static final int INF = 1000000000;
+
+    public static void FloyWarshall(int num, int size, int[][] cost) {
+        int costs[][] = new int[num + 1][num + 1];
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                if (i == j)
+                    costs[i][j] = 0;
+                else
+                    costs[i][j] = INF;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            int s = cost[i][0];
+            int e = cost[i][1];
+            int c = cost[i][2];
+            costs[s][e] = Math.min(costs[s][e], c);
+        }
+
+        for (int mid = 1; mid <= num; mid++) {
+            for (int i = 1; i <= num; i++) {
+                if (i == mid) continue;
+                for (int j = 1; j <= num; j++) {
+                    if (j == mid) continue;
+                    int newCost = costs[i][mid] + costs[mid][j];
+                    costs[i][j] = Math.min(costs[i][j], newCost);
+                }
+            }
+        }
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                if (costs[i][j] == INF)
+                    System.out.print("0 ");
+                else
+                    System.out.print(costs[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    static class KruskalMST {
+        public static int solution(int n, int[][] costs) {
             int answer = 0;
 
             Edge[] edges = new Edge[costs.length];
 
-            for(int i=0;i<costs.length;i++){
+            for (int i = 0; i < costs.length; i++) {
                 int[] cost = costs[i];
                 edges[i] = new Edge(cost[0], cost[1], cost[2]);
             }
@@ -107,48 +149,52 @@ public class Zip {
             });
 
             int parents[] = new int[n];
-            for(int i=0;i<n;i++){
+            for (int i = 0; i < n; i++) {
                 parents[i] = i;
             }
 
-            for(int i = 0;i<edges.length;i++){
-                if(!cycle(parents,edges[i].s,edges[i].e)) {
-                    answer+=edges[i].cost; // 총 소모비용
-                    union(parents,edges[i].s,edges[i].e);
+            for (int i = 0; i < edges.length; i++) {
+                if (!cycle(parents, edges[i].s, edges[i].e)) {
+                    answer += edges[i].cost; // 총 소모비용
+                    union(parents, edges[i].s, edges[i].e);
                     // 두 정점을 연결
                 }
             }
 
             return answer;
         }
-        static int getParent(int arr[], int i){
-            if(arr[i] == i){
+
+        static int getParent(int arr[], int i) {
+            if (arr[i] == i) {
                 return i;
             }
             return arr[i] = getParent(arr, arr[i]);
         }
-        static void union(int arr[], int a, int b){
+
+        static void union(int arr[], int a, int b) {
             int pa = getParent(arr, a);
             int pb = getParent(arr, b);
-            if(pa < pb)
+            if (pa < pb)
                 arr[pb] = pa;
             else
                 arr[pa] = pb;
 
         }
-        static boolean cycle(int arr[], int a, int b){
+
+        static boolean cycle(int arr[], int a, int b) {
             int pa = getParent(arr, a);
             int pb = getParent(arr, b);
 
-            if(pa == pb)
+            if (pa == pb)
                 return true;
             else
                 return false;
         }
-        static class Edge{
+
+        static class Edge {
             int s, e, cost;
 
-            Edge(int s, int e, int cost){
+            Edge(int s, int e, int cost) {
                 this.s = s;
                 this.e = e;
                 this.cost = cost;
@@ -157,21 +203,37 @@ public class Zip {
     }
 
     public static void main(String[] args) {
-        int[][] a = {{1, 2}, {4, 3}};
-        Zip zip = new Zip();
-        int[][] b = zip.clockwiseRotatedBox(a);
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b[i].length; j++) {
-                System.out.print(b[i][j] + " ");
-            }
-            System.out.println();
-        }
+//        int[][] a = {{1, 2}, {4, 3}};
+//        Zip zip = new Zip();
+//        int[][] b = zip.clockwiseRotatedBox(a);
+//        for (int i = 0; i < b.length; i++) {
+//            for (int j = 0; j < b[i].length; j++) {
+//                System.out.print(b[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        KruskalMST mst = new KruskalMST();
+//        int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5},
+//                {1, 3, 1}, {2, 3, 8}};
+//        int sol = mst.solution(4, costs);
+//        System.out.println(sol);
 
-        KruskalMST mst = new KruskalMST();
-        int[][] costs = {{0,1,1},{0,2,2}, {1,2,5},
-                {1,3,1},{2,3,8}};
-        int sol = mst.solution(4, costs);
-        System.out.println(sol);
+        int costs[][] = {{1, 2, 2},
+                {1, 3, 3},
+                {1, 4, 1},
+                {1, 5, 10},
+                {2, 4, 2},
+                {3, 4, 1},
+                {3, 5, 1},
+                {4, 5, 3},
+                {3, 5, 10},
+                {3, 1, 8},
+                {1, 4, 2},
+                {5, 1, 7},
+                {3, 4, 2},
+                {5, 2, 4}};
+        FloyWarshall(5, 14, costs);
     }
 
 }
